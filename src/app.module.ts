08dab41f115835e62796c900923e74b7491eb14d
sunnatillopy <sunnatillosharipov1905@gmail.com>
2024-07-27 22:37:54 +0500

    deleted app/controller and service file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UploadFileModule } from './upload-file/upload-file.module';
 import { CourseModule } from './course/course.module';
@@ -9,7 +7,5 @@ import { UserCourseModule } from './user-course/user-course.module';
 
 @Module({
   imports: [AuthModule, UploadFileModule, CourseModule, CourseFileModule, UserCourseModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
